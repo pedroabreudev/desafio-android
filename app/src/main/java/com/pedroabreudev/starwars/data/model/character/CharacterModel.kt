339d0f6f01f3205ec18c96a2918e5e6cdf9ee1bd
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-24 11:10:33 -0400

    Create CharacterModel e CharacterModelData

---
+++ app/src/main/java/com/pedroabreudev/starwars/data/model/character/CharacterModel.kt
@@ -0,0 +1,8 @@
+package com.pedroabreudev.starwars.data.model.character
+
+data class CharacterModel(
+    val name: String,
+    val height: String,
+    val gender: String,
+    val mass: String
+)
---
+++ app/src/main/java/com/pedroabreudev/starwars/data/model/character/CharacterModelData.kt
@@ -0,0 +1,5 @@
+package com.pedroabreudev.starwars.data.model.character
+
+data class CharacterModelData(
+    val results: List<CharacterModel>
+)

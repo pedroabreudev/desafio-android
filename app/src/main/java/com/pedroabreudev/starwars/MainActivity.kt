9370aca5bbaffe6f2f53ed1600f8b9084e8c3844
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-24 18:07:50 -0400

    update files.

--- app/src/main/java/com/pedroabreudev/starwars/MainActivity.kt
+++ app/src/main/java/com/pedroabreudev/starwars/MainActivity.kt
@@ -2,10 +2,32 @@ package com.pedroabreudev.starwars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.pedroabreudev.starwars.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {  }
+        }
+
     }
 }

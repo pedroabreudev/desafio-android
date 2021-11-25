719e12f02b0010a0743280659daec314fc8b29c5
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-25 18:08:00 -0400

    update ListCharacterViewModel

--- app/src/main/java/com/pedroabreudev/starwars/ui/list/ListCharacterFragment.kt
+++ app/src/main/java/com/pedroabreudev/starwars/ui/list/ListCharacterFragment.kt
@@ -1,16 +1,62 @@
 package com.pedroabreudev.starwars.ui.list
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pedroabreudev.starwars.databinding.FragmentListCharacterBinding
+import com.pedroabreudev.starwars.ui.adapter.CharacterAdapter
 import com.pedroabreudev.starwars.ui.base.BaseFragment
+import com.pedroabreudev.starwars.ui.state.ResourceState
+import com.pedroabreudev.starwars.util.hide
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ListCharacterFragment : BaseFragment<FragmentListCharacterBinding, ListCharacterViewModel>() {
 
     override val viewModel: ListCharacterViewModel by viewModels()
+    private val characterAdapter by lazy { CharacterAdapter() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        collectObserver()
+
+    }
+
+    private fun collectObserver() = lifecycleScope.launch {
+        viewModel.list.collect { resource ->
+            when (resource) {
+                is ResourceState.Success -> {
+                    resource.data?.let { values ->
+                        binding.pbCircular.hide()
+
+                    }
+                }
+                is ResourceState.Error -> {
+                }
+                is ResourceState.Loading -> {
+                }
+                else -> {
+                }
+            }
+
+        }
+
+    }
+
+
+    private fun setupRecyclerView() = with(binding) {
+        rvCharacters.apply {
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
 
     override fun getViewBinding(
         inflater: LayoutInflater,

24272e05e577250bb5518617af7b826d71de83db
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-26 10:33:28 -0400

    Create SplashActivity and activity_splash

---
+++ app/src/main/java/com/pedroabreudev/starwars/ui/activity/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.pedroabreudev.starwars.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pedroabreudev.starwars.MainActivity
+import com.pedroabreudev.starwars.R
+import com.pedroabreudev.starwars.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setupSplash()
+        supportActionBar?.hide()
+    }
+
+    private fun setupSplash() = with(binding) {
+        tvSplash.alpha = 0f
+        tvSplash.animate().setDuration(1000).alpha(1f).withEndAction {
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+
+}
